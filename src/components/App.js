84f1b43d320b22039048817ed3c2ceabc57c334e
sunilkushwaha <sunilkushwaha055>
2023-06-19 20:06:09 +0530

    chngs

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,15 @@
-import React, { Component, useState } from "react";
+import { Component, useState } from "react";
 import "../styles/App.css";
 // import {Counter} from "./Counter";
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
-
-const incrementHandler = ()=>{
-    setCount(count+1);
-}
-
-const decrementHandler=()=>{
-    setCount(count -1);
-}
-
-const resethandler= ()=>{
-    setCount(0);
-}
-
-
-
   return (
     <>
-      <div id="main"></div>
-
-      <div id="data-ns-test" >Counter opration</div>
-      <h5 id="">{count}</h5>
-
-      <button onClick={incrementHandler}>+</button>
-      <button onClick={decrementHandler}>-</button>
-      <button onClick={resethandler}>reset</button>
+      <div id="main">
+      <h1 data-ns-test="project-name">Dream Project</h1>
+      <p data-ns-test="project-decription">Project Description</p>
+      </div>
     </>
   );
 };
